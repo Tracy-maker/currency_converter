1e0112c84a91112e97d06014cbdf3aa995d9d4a7
Tracy-maker <ydlvns@gmail.com>
2024-10-01 18:12:08 +1000

    fix errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
-  pageExtensions: ["js", "jsx", "ts", "tsx"],
-
-  basePath: "/src",
-};
+const nextConfig = {};
 
 export default nextConfig;

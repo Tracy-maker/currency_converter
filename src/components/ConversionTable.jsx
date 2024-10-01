1354685d12a96489db40ba06b95580702b3e8cab
Tracy-maker <ydlvns@gmail.com>
2024-10-01 14:11:53 +1000

    add the UI elements of the components

--- src/components/ConversionTable.jsx
+++ src/components/ConversionTable.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const ConversionTable = () => {
   return (
-    <div>ConversionTable</div>
-  )
-}
+    <div className="mt-4 grid grid-cols-2 gap-4">
+      <div className="p-4 bg-gray-100 rounded shadow">
+        <p className="text-xl font-bold">11</p>
+        <p>22</p>
+      </div>
+    </div>
+  );
+};
 
-export default ConversionTable
+export default ConversionTable;
--- src/components/CurrencyConverter.jsx
+++ src/components/CurrencyConverter.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import ConversionTable from "./ConversionTable";
 
 const CurrencyConverter = () => {
   return (
-    <div>CurrencyConverter</div>
-  )
-}
+    <div className="mt-4">
+      <input
+        className="border p-2 rounded w-full"
+        placeholder="Enter amount in AUD"
+      />
+      <ConversionTable />
+    </div>
+  );
+};
 
-export default CurrencyConverter
+export default CurrencyConverter;

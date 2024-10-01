a5ad0e61a0ae6eed61fa0c44788116db8038a60b
Tracy-maker <ydlvns@gmail.com>
2024-10-01 17:53:22 +1000

    fix the deploy errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  pageExtensions: ["js", "jsx", "ts", "tsx"],
+
+  basePath: "/src",
+};
 
 export default nextConfig;
